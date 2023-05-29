d45dd0ce59e039ead5ed778bbe9bf624036abb71
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-05-29 10:49:18 -0300

    :tada: start: starting project

---
+++ src/main/java/com/agrotechfields/measureshelter/MeasureshelterApplication.java
@@ -0,0 +1,13 @@
+package com.agrotechfields.measureshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasureshelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasureshelterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrotechfields/measureshelter/MeasureshelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotechfields.measureshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasureshelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
