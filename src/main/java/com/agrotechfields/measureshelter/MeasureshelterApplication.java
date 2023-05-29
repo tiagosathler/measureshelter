5fc97aaa81f869cf02c2cce809a507b155f846a4
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-05-29 17:06:01 -0300

    :test_tube: test: assert fail - expected to fail

--- src/main/java/com/agrotechfields/measureshelter/MeasureshelterApplication.java
+++ src/main/java/com/agrotechfields/measureshelter/MeasureshelterApplication.java
@@ -3,9 +3,17 @@ package com.agrotechfields.measureshelter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The Class MeasureshelterApplication.
+ */
 @SpringBootApplication
 public class MeasureshelterApplication {
 
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
   public static void main(String[] args) {
     SpringApplication.run(MeasureshelterApplication.class, args);
   }
