bcf6414b1be51ecce6e621913a9dc27862b1f740
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-06-06 20:59:44 -0300

    :sparkles: feat: create 'Role' enum

---
+++ src/main/java/com/agrotechfields/measureshelter/domain/Role.java
@@ -0,0 +1,14 @@
+package com.agrotechfields.measureshelter.domain;
+
+/**
+ * The Enum Role.
+ */
+public enum Role {
+
+  /** The role admin. */
+  ROLE_ADMIN,
+  /** The role user. */
+  ROLE_USER,
+  /** The role isle. */
+  ROLE_ISLE;
+}
