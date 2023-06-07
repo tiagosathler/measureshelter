4a26b02be8d2312f0ddd45c0148864595e37fdd2
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-06-06 21:12:46 -0300

    :sparkles: feat: add some exception handlers

--- src/main/java/com/agrotechfields/measureshelter/exception/handler/ExceptionHandlerController.java
+++ src/main/java/com/agrotechfields/measureshelter/exception/handler/ExceptionHandlerController.java
@@ -4,8 +4,12 @@ import com.agrotechfields.measureshelter.exception.EntityAlreadyExistsException;
 import com.agrotechfields.measureshelter.exception.EntityNotFoundException;
 import com.agrotechfields.measureshelter.exception.NotPermittedException;
 import com.agrotechfields.measureshelter.exception.payload.ErrorPayload;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -68,6 +72,45 @@ public class ExceptionHandlerController {
   public ResponseEntity<ErrorPayload> handleNotPermitted(NotPermittedException e) {
     HttpStatus httpStatus = HttpStatus.NOT_ACCEPTABLE;
     String msg = e.getMessage() + " is not permitted or not working to do this";
+    return buildResponse(msg, httpStatus);
+  }
+
+  /**
+   * Handle bad credentials.
+   *
+   * @param e the e
+   * @return the response entity
+   */
+  @ExceptionHandler(BadCredentialsException.class)
+  public ResponseEntity<ErrorPayload> handleBadCredentials(BadCredentialsException e) {
+    HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+    String msg = "Invalid username or password";
+    return buildResponse(msg, httpStatus);
+  }
+
+  /**
+   * Handle token exception.
+   *
+   * @param e the e
+   * @return the response entity
+   */
+  @ExceptionHandler(TokenExpiredException.class)
+  public ResponseEntity<ErrorPayload> handleTokenException(Exception e) {
+    HttpStatus httpStatus = HttpStatus.FORBIDDEN;
+    String msg = e.getMessage();
+    return buildResponse(msg, httpStatus);
+  }
+
+  /**
+   * Handle invalid token.
+   *
+   * @param e the e
+   * @return the response entity
+   */
+  @ExceptionHandler({SignatureVerificationException.class, JWTDecodeException.class})
+  public ResponseEntity<ErrorPayload> handleInvalidToken(Exception e) {
+    HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+    String msg = "Invalid token";
     return buildResponse(msg, httpStatus);
   }
 
