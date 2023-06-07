a0e5bd6e864ff018514e7f8ea45570454eb3514c
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-06-06 21:13:08 -0300

    :sparkles: feat: add scan components

--- src/main/java/com/agrotechfields/measureshelter/MeasureshelterApplication.java
+++ src/main/java/com/agrotechfields/measureshelter/MeasureshelterApplication.java
@@ -3,11 +3,13 @@ package com.agrotechfields.measureshelter;
 import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * The Class MeasureshelterApplication.
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.agrotechfields.measureshelter"})
 public class MeasureshelterApplication {
 
   /**
