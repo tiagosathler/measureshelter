8e12c22c4ec500ba99577674b22fc9938c9fce62
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-06-12 20:23:02 -0300

    :recycle: refactor: change the 'findBySerialNumber' method return to 'Optional'

--- src/main/java/com/agrotechfields/measureshelter/repository/IsleRepository.java
+++ src/main/java/com/agrotechfields/measureshelter/repository/IsleRepository.java
@@ -1,6 +1,7 @@
 package com.agrotechfields.measureshelter.repository;
 
 import com.agrotechfields.measureshelter.domain.Isle;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,11 +11,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IsleRepository extends MongoRepository<Isle, String> {
 
+
   /**
    * Find by serial number.
    *
    * @param serialNumber the serial number
-   * @return the isle
+   * @return the optional
    */
-  public Isle findBySerialNumber(String serialNumber);
+  public Optional<Isle> findBySerialNumber(String serialNumber);
 }
