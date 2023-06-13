0a1c432ca8b4dfb7927f30afb6ea0ecfc8777ca9
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-06-13 15:40:06 -0300

    :recycle: refactor: change 'findByIsle' to 'findByIsleId' method using isleId the ObjectId type

--- src/main/java/com/agrotechfields/measureshelter/repository/MeasureRepository.java
+++ src/main/java/com/agrotechfields/measureshelter/repository/MeasureRepository.java
@@ -1,6 +1,5 @@
 package com.agrotechfields.measureshelter.repository;
 
-import com.agrotechfields.measureshelter.domain.Isle;
 import com.agrotechfields.measureshelter.domain.Measure;
 import java.util.List;
 import org.bson.types.ObjectId;
@@ -10,12 +9,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * The Interface MeasureRepository.
  */
 public interface MeasureRepository extends MongoRepository<Measure, ObjectId> {
-  
+
   /**
    * Find by isle.
    *
-   * @param isle the isle
+   * @param isleId the isle object id
    * @return the list
    */
-  public List<Measure> findByIsle(Isle isle);
+  public List<Measure> findByIsleId(ObjectId isleId);
 }
