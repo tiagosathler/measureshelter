7790e8c7cdd07d5511008c8061d85b3585ae5e4c
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-06-13 14:36:32 -0300

    :recycle: refactor: change id attribute to ObjectId

--- src/main/java/com/agrotechfields/measureshelter/repository/MeasureRepository.java
+++ src/main/java/com/agrotechfields/measureshelter/repository/MeasureRepository.java
@@ -3,12 +3,13 @@ package com.agrotechfields.measureshelter.repository;
 import com.agrotechfields.measureshelter.domain.Isle;
 import com.agrotechfields.measureshelter.domain.Measure;
 import java.util.List;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * The Interface MeasureRepository.
  */
-public interface MeasureRepository extends MongoRepository<Measure, String> {
+public interface MeasureRepository extends MongoRepository<Measure, ObjectId> {
   
   /**
    * Find by isle.
