ca21ff53ee1613a2c5fd5e5f0d61f4e31a71487c
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-06-14 16:45:15 -0300

    :test_tube: test: add login test using a valid username

--- src/test/java/com/agrotechfields/measureshelter/MeasureshelterApplicationTest.java
+++ src/test/java/com/agrotechfields/measureshelter/MeasureshelterApplicationTest.java
@@ -1,27 +1,84 @@
 package com.agrotechfields.measureshelter;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import com.agrotechfields.measureshelter.domain.Role;
+import com.agrotechfields.measureshelter.domain.User;
+import com.agrotechfields.measureshelter.dto.request.AuthDto;
+import com.agrotechfields.measureshelter.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 /**
  * The Class MeasureshelterApplicationTests.
  */
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MeasureshelterApplicationTest {
+  @Autowired
+  private MockMvc mockMvc;
 
-  /**
-   * Context loads.
-   */
-  @Test
-  @DisplayName("ContextLoads")
-  void contextLoads() {
-    assertEquals(1, 1);
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @SpyBean
+  private UserRepository userRepository;
+
+  @Container
+  static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:5");
+
+  @DynamicPropertySource
+  static void mongoDbProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
   }
 
+  @BeforeEach
+  public void setup() {
+    userRepository.deleteAll();;
+  }
+
+  @Test
+  @Order(1)
+  @DisplayName("1. Login using a valid username")
+  void loginUsingAValidUsername() throws Exception {
+    insertUser();
+
+    AuthDto authDto = new AuthDto("admin", "pass");
+    String body = objectMapper.writeValueAsString(authDto);
+
+    mockMvc.perform(post("/login").contentType(MediaType.APPLICATION_JSON).content(body))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+        .andExpect(jsonPath("$.token").isString());
+  }
+
+  private void insertUser() {
+    String encodedPassword = passwordEncoder.encode("pass");
+    User user = new User(null, "admin", encodedPassword, Role.ROLE_ADMIN);
+    userRepository.save(user);
+  }
 }
